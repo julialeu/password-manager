b7e242f18586e0643167787f140c83054f4551a7
Julia Leuenberger <leuenberger2101@gmail.com>
2025-09-25 16:59:15 +0200

    Change env and config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./password_manager.db"
 
     # Seguridad y JWT
-    SECRET_KEY: str = "a_very_secret_key"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     ENCRYPTION_KEY: str
